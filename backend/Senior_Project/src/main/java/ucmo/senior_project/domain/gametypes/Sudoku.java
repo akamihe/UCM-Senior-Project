5347fb4b4fe8ea70ba5281348e038de9bd87b218
Yomi <allazham@hotmail.com>
2024-03-11 00:16:41 -0500

    commit for battleship backend (DOES NOT  work with front end yet).

--- backend/Senior_Project/src/main/java/ucmo/senior_project/domain/gametypes/Sudoku.java
+++ backend/Senior_Project/src/main/java/ucmo/senior_project/domain/gametypes/Sudoku.java
@@ -6,10 +6,14 @@ import ucmo.senior_project.domain.TempUser;
 import ucmo.senior_project.resource.GameData;
 import ucmo.senior_project.resource.gametypes.SudokuData;
 
-public class Sudoku  implements Game {
+public class Sudoku implements Game {
+    public static long TIME_TOTAL = 60*5; // 5 minutes
+    public static long TIME_START = 10; //10 seconds
+    private long endTime = 0;
+    private long lastUpdate = 0;
     @Override
     public GameData getGameData(TempUser user) {
-        return new SudokuData();
+        return new SudokuData(this.endTime);
     }
 
     @Override
@@ -19,6 +23,13 @@ public class Sudoku  implements Game {
 
     @Override
     public void updateSystem() {
-
+        if(this.endTime == 0) {
+            this.lastUpdate = System.currentTimeMillis();
+            this.endTime = (TIME_TOTAL + TIME_START) * 1000 + this.lastUpdate;
+        } else {
+            long timeNow = System.currentTimeMillis();
+            endTime -= (timeNow - this.lastUpdate);
+            this.lastUpdate = timeNow;
+        }
     }
 }
