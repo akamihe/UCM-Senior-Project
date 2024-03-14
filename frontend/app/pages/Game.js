888d91f82f8b5d353b315f0b0dc99112a4e7c09c
akamihe <akamihemichael@gmail.com>
2024-03-14 01:45:37 -0500

    Code cleanup for Battleship and Sudoku games

--- frontend/app/pages/Game.js
+++ frontend/app/pages/Game.js
@@ -1,20 +1,19 @@
-import { useState, useEffect, useMemo } from "react";
-import Sudoku from "./Sudoku";
-import Battleship from "./Battleship";
+import { useState, useEffect } from "react";
 import { FaCircleCheck, FaCircleXmark } from "react-icons/fa6";
-import SudokuPuzzle from "./SudokuPuzzle";
+import Battleship from "./Battleship";
+import Sudoku from "./Sudoku";
 
 const style = {
   container: { display: "flex", flexDirection: "column", justifyContent: "center", height: "100%" },
   gameSection: { 
     display: "flex", 
-    flexDirection: 'column', 
     overflow: "hidden",
     width: "100%", 
-    height: "100%" 
+    height: "100%",
+    flexWrap: "wrap" 
   },
-  userInfoSection: { display: "flex", borderTop: "1px solid #0004", gap: 16, padding: 16 },
-  userInfoItem: {
+  playerInfoSection: { display: "flex", borderTop: "1px solid #0004", gap: 16, padding: 16 },
+  playerInfoItem: {
     item: { 
       display: "flex", 
       alignItems: "center", 
@@ -42,7 +41,7 @@ const style = {
     flexDirection: "column", 
     height: "100%"
   },
-  gameResultsUser: {
+  gameResultsPlayer: {
     top: { display: "flex", alignItems: "center", borderBottom: "1px solid #0003", paddingBottom: 8 },
     placeNum: (color) => ({ 
       width: 29, 
@@ -67,229 +66,85 @@ const style = {
   gameInstanceContent: { transform: "scale(0.6)", transformOrigin: "top", paddingTop: 8 }
 }
 
-const initialUsers = [
-  { id: 1, name: "john_doe", },
-  { id: 2, name: "jane_doe", },
-  { id: 3, name: "jack_doe", you: true },
-  { id: 4, name: "jill_doe", }
+const COUNTDOWN_LENGTH = 10;
+const initialPlayers = [
+  { id: 1, name: "john_doe", pts: 0, done: false, ptsAwarded: 0 },
+  { id: 2, name: "jane_doe", pts: 0, done: false, ptsAwarded: 0 },
+  { id: 3, name: "jack_doe", you: true, pts: 0, done: false, ptsAwarded: 0 },
+  { id: 4, name: "jill_doe", pts: 0, done: false, ptsAwarded: 0 }
 ]
-
-// const games = ["Sudoku", "Battleship"];
-const games = ["Battleship"]
+const games = ["Sudoku", "Battleship"];
 
 const Game = () => {
-  const [count, setCount] = useState(10);
-  const [gameNum, setGameNum] = useState(-1);
-  const [isGameActive, setIsGameActive] = useState(false);
+  const [count, setCount] = useState(COUNTDOWN_LENGTH);
+  const [gameNum, setGameNum] = useState(-1); // index of current game
+  const [isGameActive, setIsGameActive] = useState(false); // flag for showing the current game
+  const [players, setPlayers] = useState(initialPlayers.map(player => ({ ...player })));
 
-  const [users, setUsers] = useState(initialUsers.map((user, idx) => ({
-    ...user,
-    pts: 0,
-    done: false,
-    eliminated: false,
-    ptsAwarded: 0,
-    board: new Array(10).fill(new Array(10).fill(false)),
-    ships: [
-      // [ [0, 3], [0, 4], [0, 5], [0, 6] ],
-      // [ [3, 1], [4, 1], [5, 1] ],
-      // [ [5, 8] ],
-      [ [8, 4], [8, 5] ],
-      // [ [8, 9], [9, 9] ]
-    ],
-    attacking: (idx + 1) % initialUsers.length
-  })));
-
-  const [turn, setTurn] = useState(0);
-  const [turnNum, setTurnNum] = useState(0);
-  const [selectedCell, setSelectedCell] = useState(null);
-
-  const sudokuPuzzle = useMemo(() => {
-    return SudokuPuzzle.generatePuzzle();
-  }, [])
-
-  const gameFinished = (
-    users.every(user => user.done) || 
-    users.filter(user => user.eliminated).length === users.length - 1
-  );
+  const playersSortedByPts = players.slice().sort((a, b) => b.pts - a.pts);
+  const game = gameNum >= 0 ? games[gameNum] : "";
+  const nextGame = games[gameNum + 1];
+  const elimination = game === "Battleship";
 
   useEffect(() => {
     if (!isGameActive)
-      // countDown(10);
-      countDown(0);
-  }, [isGameActive])
-
-  useEffect(() => {
-    if (gameFinished) {
-      setTimeout(() => {
-        setUsers(users.map(user => ({ ...user, done: false, eliminiated: false, pts: user.pts + user.ptsAwarded })));
-        setIsGameActive(false);
-      }, 3000);
-    }
-  }, [gameFinished])
+      countDown(COUNTDOWN_LENGTH);
+  // once the component inits or a game finishes, display the next game in COUNTDOWN_LENGTH seconds;
+  }, [isGameActive]) 
 
   function countDown(count) {
     setCount(count)
 
     if (count > 0) {
-      setTimeout(() => {
-        countDown(count - 1);
-      }, 1000);
+      setTimeout(() => countDown(count - 1), 1000);
     } else {
+      // once countdown finishes, display the next game, reset game state 
+      // and add the points awarded from the previous game
+      setPlayers(players.map(player => ({ ...player, done: false, pts: player.pts + player.ptsAwarded, ptsAwarded: 0 })));
       setGameNum(gameNum + 1);
       setIsGameActive(true);
     }
   }
 
-  function markUserAsDone(idx) {
-    setUsers(prev => {
-      const newUsers = prev.slice();
-      const ptsAwarded = 4 - (newUsers.filter(user => user.done).length + 1);
-      newUsers[idx] = { ...newUsers[idx], done: true, ptsAwarded };
-      return newUsers;
-    })
+  function handleGameComplete() {
+    setIsGameActive(false); // show results after game finishes
   }
-
-  function handleTurnComplete(i, j) {
-    const newUsers = users.slice();
-    const attackedIdx = users[turn].attacking;
-    const board = newUsers[attackedIdx].board.map(row => row.slice());
-    const twoLeft = newUsers.filter(user => user.eliminated).length === 2;
-
-    board[i][j] = true; 
-
-    const allShipsSunk = newUsers[attackedIdx].ships.every(ship => {
-      return ship.every(cell => board[cell[0]][cell[1]]);
-    });
-
-    newUsers[attackedIdx] = { ...newUsers[attackedIdx], board, eliminated: allShipsSunk };
-    newUsers[turn] = { 
-      ...newUsers[turn],
-      ptsAwarded: newUsers[turn].ptsAwarded + (allShipsSunk ? (twoLeft ? 2 : 1) : 0)
-    };
-
-    setTimeout(() => {
-      if (allShipsSunk)
-        newUsers[turn] = { ...newUsers[turn], attacking: newUsers[attackedIdx].attacking };
-      else
-        setTurn(attackedIdx);
-      
-      setSelectedCell(null);
-      setTurnNum(turnNum + 1);
-    }, 2500);
-    
-    setSelectedCell([i, j]);
-    setUsers(newUsers);
-  }
-
-  function getShip(ships, i, j) {
-    for (const ship of ships) {
-      for (const cell of ship) {
-        if (cell[0] === i && cell[1] === j)
-          return ship;
-      }
-    }
-
-    return null;
-  }
-
-  const attacked = users[users[turn].attacking];
 
   function renderGame(game) {
-    if (game === "Sudoku") {
-      return (
-        <>
-          <div style={{ ...style.gameInstanceRow, borderBottom: "1px solid #0002" }}>
-            <div style={style.gameInstanceCellL}>
-              <div style={style.gameInstanceContent}>
-                <Sudoku puzzle={sudokuPuzzle} onComplete={() => markUserAsDone(0)} />
-              </div>
-            </div>
-            <div style={style.gameInstanceCellR}>
-              <div style={style.gameInstanceContent}>
-                <Sudoku puzzle={sudokuPuzzle} onComplete={() => markUserAsDone(1)} />
-              </div>
-            </div>
-          </div>
-          <div style={style.gameInstanceRow}>
-            <div style={style.gameInstanceCellL}>
-              <div style={style.gameInstanceContent}>
-                <Sudoku puzzle={sudokuPuzzle} onComplete={() => markUserAsDone(2)} />
-              </div>
-            </div>
-            <div style={style.gameInstanceCellR}>
-              <div style={style.gameInstanceContent}>
-                <Sudoku puzzle={sudokuPuzzle} onComplete={() => markUserAsDone(3)} />
-              </div>
-            </div>
-          </div>
-        </>
-      )
-    } else if (game === "Battleship") {
-      const instances = users.map(user => (
-        <Battleship
-          turn={{
-            attacker: users[turn],
-            attacked,
-            next: attacked.eliminated ? users[attacked.attacking] : attacked, 
-            turn: turnNum,
-            selectedCell
-          }}
-          onTurnComplete={handleTurnComplete}
-          user={user}
-        />
-      ))
-
-      return (
-        <>
-          <div style={{ ...style.gameInstanceRow, borderBottom: "1px solid #0002" }}>
-            <div style={style.gameInstanceCellL}>
-              <div style={style.gameInstanceContent}>
-                {instances[0]}
-              </div>
-            </div>
-            <div style={style.gameInstanceCellR}>
-              <div style={style.gameInstanceContent}>
-                {instances[1]}
-              </div>
-            </div>
-          </div>
-          <div style={style.gameInstanceRow}>
-            <div style={style.gameInstanceCellL}>
-              <div style={style.gameInstanceContent}>
-                {instances[2]}
-              </div>
-            </div>
-            <div style={style.gameInstanceCellR}>
-              <div style={style.gameInstanceContent}>
-                {instances[3]}
-              </div>
-            </div>
-          </div>
-        </>
-      )
+    switch (game) {
+      case "Sudoku":
+        return <Sudoku onGameComplete={handleGameComplete} onSetPlayer={setPlayer} players={players} />
+      case "Battleship":
+        return <Battleship onGameComplete={handleGameComplete} onSetPlayer={setPlayer} players={players} />
+      default:
+        return null;
     }
-  
-    return null;
   }
 
-  const usersSortedByPts = users.slice().sort((a, b) => b.pts - a.pts);
-  const game = gameNum >= 0 ? games[gameNum] : "";
-  const nextGame = games[gameNum + 1];
+  function setPlayer(player) {
+    setPlayers(prev => {
+      const newPlayers = prev.slice();
+      const idx = newPlayers.findIndex(u => u.id === player.id);
+      newPlayers[idx] = player;
+      return newPlayers
+    });
+  }
 
   return (
     <div style={style.container}>
       {isGameActive ? (
         <>
-          <div style={style.gameSection}>
-            {renderGame(game)}
-          </div>
-          <div style={style.userInfoSection}>
-            {users.map((user, idx) => (
-              <div key={idx} style={style.userInfoItem.item}>
-                <span style={{ flex: 1, marginRight: 8 }}>{user.name}</span>
-                <span style={style.userInfoItem.pts}>{user.pts} pts</span>
-                {user.done && <FaCircleCheck style={style.userInfoItem.doneIcon} />}
-                {user.eliminated && <FaCircleXmark style={{ ...style.userInfoItem.doneIcon, color: "red" }} />}
+          <div style={style.gameSection}>{renderGame(game)}</div>
+          <div style={style.playerInfoSection}>
+            {players.map((player, idx) => (
+              <div key={idx} style={style.playerInfoItem.item}>
+                <span style={{ flex: 1, marginRight: 8 }}>{player.name}</span>
+                <span style={style.playerInfoItem.pts}>{player.pts + player.ptsAwarded} pts</span>
+                {player.done && 
+                  (elimination 
+                    ? <FaCircleXmark style={{ ...style.playerInfoItem.doneIcon, color: "red" }} />
+                    : <FaCircleCheck style={style.playerInfoItem.doneIcon} />)
+                }
               </div>
             ))}
           </div>
@@ -304,20 +159,20 @@ const Game = () => {
       ) : (
         <div style={style.gameResultsContainer}>
           <div style={{ width: 600 }}>
-            {usersSortedByPts.map((user, idx) => {
+            {playersSortedByPts.map((player, idx) => {
               const placementColor = ["#ddc700", "#aaaaaa", "#905923", "#000000"][idx];
 
               return (
                 <div key={idx} style={{ marginBottom: 16 }}>
-                  <div style={style.gameResultsUser.top}>
-                    <span style={style.gameResultsUser.placeNum(placementColor)}>{idx + 1}</span>
-                    <span style={style.gameResultsUser.name}>{user.name}</span>
+                  <div style={style.gameResultsPlayer.top}>
+                    <span style={style.gameResultsPlayer.placeNum(placementColor)}>{idx + 1}</span>
+                    <span style={style.gameResultsPlayer.name}>{player.name}</span>
                     <div style={{ textAlign: "right", lineHeight: 1 }}>
-                      <p style={style.gameResultsUser.prevPoints}>{user.pts - user.ptsAwarded} pts</p>
-                      <p style={style.gameResultsUser.ptsAwarded}>+{user.ptsAwarded} pts</p>
+                      <p style={style.gameResultsPlayer.prevPoints}>{player.pts} pts</p>
+                      <p style={style.gameResultsPlayer.ptsAwarded}>+{player.ptsAwarded} pts</p>
                     </div>
                   </div>
-                  <p style={style.gameResultsUser.pts}>{user.pts} pts</p>
+                  <p style={style.gameResultsPlayer.pts}>{player.pts + player.ptsAwarded} pts</p>
                 </div>
               )
             })}
